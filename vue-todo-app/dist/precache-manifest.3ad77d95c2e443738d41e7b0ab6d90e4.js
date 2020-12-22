702aa686c29dc1a5274e850e2e6a699e68f07a5c
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2020-12-22 13:58:01 +0100

    vue add

---
+++ vue-todo-app/dist/precache-manifest.3ad77d95c2e443738d41e7b0ab6d90e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6eb3d038e7ab27e266c",
+    "url": "/css/app.bf77f933.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a3453dc59305f074448a3ff7aab5ed90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6eb3d038e7ab27e266c",
+    "url": "/js/app.f1dd8298.js"
+  },
+  {
+    "revision": "e09ddbe555c3e1a14f23",
+    "url": "/js/chunk-vendors.121afc26.js"
+  },
+  {
+    "revision": "b497742f054eed334b04e26b9ca4f41b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
